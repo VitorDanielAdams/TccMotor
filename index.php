27570851c0a9c1152cf21e4fdf5fd7759d0e60ca
VitorDanielAdams <vitor.adams@outlook.com>
2021-09-18 13:20:26 -0300

    005

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="CSS/style.css" />
+    <link rel="icon" href="images/icon.jpg">
+
     <title>Pagina Inicial</title>
 </head>
 <body>
--- login.php
+++ login.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;500&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="CSS/style.css"/>
+    <link rel="stylesheet" type="text/css" href="CSS/styleLogin.css"/>
     <link rel="icon" href="images/icon.jpg">
     
     <title>Tela de login </title>
