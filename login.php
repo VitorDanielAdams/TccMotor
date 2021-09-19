d5b547edf997523a100023f42f04367e463c1d90
VitorDanielAdams <vitor.adams@outlook.com>
2021-09-18 21:27:48 -0300

    008

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+require_once 'CLASSES/usuarios.php';
+$u = new Usuario;
+include 'CLASSES/conecta.php';
 ?>
 
 <!DOCTYPE html>
@@ -30,7 +33,7 @@
                 <div class="input">
                     <label>Nome</label>
                     <input value="<?php if(isset($_COOKIE[""])) { echo $_COOKIE[""]; } ?>" 
-                    type="text" name="user" id="user">
+                    type="text" name="nome" id="nome">
                     <small></small>
                 </div>
                 <div class="input">
@@ -50,7 +53,59 @@
             </form>
         </div>
     </div>
+    <script src="scriptlogin.js"></script>
 </body>
 <?php
+if(isset($_POST['logar'])){
+    $nome = addslashes($_POST['nome']);
+    $codigo = addslashes($_POST['codigo']);
+
+    if(!empty($nome) && !empty($codigo)){
+        if($u->msgErro == ""){
+            if($u->logar($nome,$codigo)){
+                if(!empty($_POST['remeber'])){
+                    setcookie("nome",$nome,time()+ (10 * 365 * 24 * 60 * 60));
+                    setcookie ("codigo",$codigo,time()+ (10 * 365 * 24 * 60 * 60));
+                } else {
+                    if(isset($_COOKIE["nome"])){  
+                        setcookie ("nome","");  
+                    }  
+                    if(isset($_COOKIE["codigo"])){  
+                        setcookie ("codigo","");  
+                    }  
+                }
+                if($_SESSION['rol'] == 0){
+                    header("location: admin.php");
+                } else if($_SESSION['rol'] == 1) {
+                    header("location: homepage.php");
+                }
+            } else {
+                ?>
+                <script>
+                    setErrorFor(inputName," ");
+                    setErrorFor(inputCod,"Usuário e/ou senha incorretos");
+                </script>
+                <?php
+            }
+        } else {
+            ?>
+            <div class="erro">
+                <small><?php echo "Erro: ".$u->msgErro; ?></small>
+            </div>
+            <?php
+        }
+    } else {
+        ?>
+        <script>
+           form.addEventListener('submit', (e) => {
+                checkInputs();
+                if(!checkInputs()){
+                    e.preventDefault();
+                }
+            });
+        </script>
+        <?php
+    }
+}
 ?>
 </html>
